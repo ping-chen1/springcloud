4a1b396bb5f7fba94e03b4e3b2ef594f3f377682
cp <826052402@qq.com>
2022-12-19 08:52:45 +0800

    submit

---
+++ consul/src/main/java/com/cp/ConsulMain.java
@@ -0,0 +1,12 @@
+package com.cp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulMain.class,args);
+    }
+}
